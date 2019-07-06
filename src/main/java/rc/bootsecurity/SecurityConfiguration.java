9d2b4e20fe015d87d7d30af084e72904bfce586e
TomaEduard <eduard.daniel.toma@gmail.com>
2019-07-06 12:42:50 +0300

    Generate SSL and enable HTTP/SSL and redirect http to https:8443

--- src/main/java/rc/bootsecurity/SecurityConfiguration.java
+++ src/main/java/rc/bootsecurity/SecurityConfiguration.java
@@ -17,19 +17,41 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN")
+
+                .withUser("admin")
+                .password(passwordEncoder().encode("admin123"))
+                .roles("ADMIN").authorities("ACCESS_TEST1", "ACCESS_TEST2")
+
                 .and()
-                .withUser("1asd").password(passwordEncoder().encode("1asd")).roles("USER");
+                .withUser("1asd")
+                .password(passwordEncoder().encode("1asd"))
+                .roles("USER")
+
+                .and()
+                .withUser("manager")
+                .password(passwordEncoder().encode("manager123"))
+                .roles("MANAGER").authorities("ACCESS_TEST1");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().authenticated()
+                .antMatchers("/index.html").permitAll()
+                .antMatchers("/profile/**").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/management/**").hasAnyRole("ADMIN", "MANAGER")
+                .antMatchers("/api/public/test1").hasAuthority("ACCESS_TEST1")
+                .antMatchers("/api/public/test2").hasAuthority("ACCESS_TEST2")
                 .and()
                 .httpBasic();
+
     }
+//                .authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .httpBasic();
+
 
     @Bean
     PasswordEncoder passwordEncoder() {
